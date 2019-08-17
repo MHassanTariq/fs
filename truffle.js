5266840aad974e199c5a0773a5e05aafe070f573
Faried Nawaz <faried@gmail.com>
2019-08-18 03:11:27 +0500

    truffle.js: enable optimizer.

--- truffle.js
+++ truffle.js
@@ -1,19 +1,29 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 module.exports = {
-  networks: {
-    development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: '*',
-      gas: 6721975
+
+    plugins: ["truffle-security"],
+
+    networks: {
+        development: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+            },
+            network_id: '*',
+            gas: 6721975,
+            gasPrice: 20000000000
+        }
+    },
+    compilers: {
+        solc: {
+            version: "0.5.11",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                }
+            }
+        }
     }
-  },
-  compilers: {
-    solc: {
-      version: "0.5.11"
-    }
-  }
 };
